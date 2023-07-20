77b738ca0e6ac3cc80a5a9d64eed4dfedd7468b9
Mubshirk <mubshirk875@gmail.com>
2023-07-21 03:07:58 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// const hamburger = document.querySelector('.hamburger');
+// const features = document.querySelector('.features');
+
+// document.querySelector('.hamburger')
+// .addEventListener('click', () => {
+//     if(features){}
+// } );
